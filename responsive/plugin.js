7a47b583e2e9c360c5882c8af4e22dbf8bd5fe1f
mysticmiz <70727702+mysticmiz@users.noreply.github.com>
2020-10-12 16:51:59 -0700

    mmmmm

--- responsive/plugin.js
+++
@@ -1,5 +0,0 @@
-var frmvalidator=new
-Validator("contactform");
-frmvalidator.addValidation("name","req","mystic");
-frmvalidator.addValidation("email","req","www.michaeldaniel7@gmail.com"):
-frmvalidator.addValidation("email","email"www.michaeldaniel7@gmail.com");
